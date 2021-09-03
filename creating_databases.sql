b377e4247779c3e7abc5a7aa5e2948aedb55bcbf
Linh Quach <Linh.M.Quach1@gmail.com>
2021-09-02 21:31:58 -0500

    initial add

---
+++ creating_databases.sql
@@ -0,0 +1,12 @@
+--- start sql in terminal command/ Start the CLI:
+mysql-ctl cli
+
+--- list current databases in mysql server/List available databases:
+show databases;
+
+--- Create hello_world_db database
+CREATE DATABASE hello_world_db
+
+--- verify database was created
+show databases;
+
