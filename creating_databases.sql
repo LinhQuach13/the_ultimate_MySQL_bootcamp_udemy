4372cbd353709692e038e08a5864f082ae1a7e5d
Linh Quach <Linh.M.Quach1@gmail.com>
2021-09-02 21:52:19 -0500

    code updated

--- creating_databases.sql
+++ creating_databases.sql
@@ -1,3 +1,5 @@
+
+---------------Creating Databases-----------
 --- start sql in terminal command/ Start the CLI:
 mysql-ctl cli
 
@@ -16,10 +18,10 @@ CREATE DATABASE testing_db;
 --- verify database was created
 show databases;
 
-
+--------------Dropping Databases-----------------
 --- drop/delete databases 
   --- Note: drop in sql is sql's version of "delete"
-  
+
 --- drop hello_world_db
   DROP DATABASE hello_world_db;
 
@@ -29,6 +31,27 @@ show databases;
 
 --- drop testing_db
   DROP DATABASE testing_db;
+
+--- verify database was dropped
+show databases;
+
+
+----------------Using Databases------------------
+
+--- Create dog_walking_app database
+CREATE DATABASE dog_walking_app;
+
+--- verify database was created
+show databases;
+
+--- use newly created database
+USE dog_walking_app;
+
+--- To verify what database we are using/ will tell which database you are currently using
+SELECT database();
+
+--- drop dog_walking_app database
+DROP DATABASE dog_walking_app;
 
 --- verify database was dropped
 show databases;
